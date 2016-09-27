53ff2c4151b978d5947c50f8c5848f75ab4aa8a1
Harley Rowland <hjr7@kent.ac.uk>
2016-09-27 16:37:23 +0100

    Change server.js

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+var port = process.env.PORT || 5000;
+
+app.listen(port, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-
