f004942698473c51da4fc1185a2db51130ffb22e
Harley Rowland <hjr7@kent.ac.uk>
2016-09-27 16:40:56 +0100

    Change server.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function (req, res) {
+  res.send('Hello Juu and Andy!');
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
