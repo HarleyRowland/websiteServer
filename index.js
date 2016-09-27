8071cd836f1bb2f0ddf0e824226ecca3093c3835
Harley Rowland <hjr7@kent.ac.uk>
2016-09-27 16:39:18 +0100

    Change server.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-var port = process.env.PORT || 5000;
 
-app.listen(port, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
