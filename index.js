0906187254219ed7f00eb4f57694877494048a36
Harley Rowland <hjr7@kent.ac.uk>
2016-09-27 16:59:11 +0100

    Final Check

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Juu and Andy!');
+  res.send('Hello World!');
 });
 
 app.set('port', (process.env.PORT || 5000));
