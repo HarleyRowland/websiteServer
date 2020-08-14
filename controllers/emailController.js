09889dc3888bc13fd40241f82b39906a129c8f51
Harley Rowland <harleyrowland17@gmail.com>
2020-08-14 14:30:44 +0100

    Update emails

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,23 +1,23 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 var config = require('../config.js');
-var xoauth2 = require('xoauth2');
 var httpStatus = require('http-status');
+
 
 module.exports = {
   sendEmail: function(sendTo, name, email, number, subject, body, callback){
 
     // login
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
         auth: {
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: config.gmail.user,
-                clientId: config.gmail.clientId,
-                clientSecret: config.gmail.clientSecret,
-                refreshToken: config.gmail.refreshToken,
-                accessToken: config.gmail.accessToken
-            })
+            type: 'OAuth2',
+            user: config.gmail.user,
+            clientId: config.gmail.clientId,
+            clientSecret: config.gmail.clientSecret,
+            refreshToken: config.gmail.refreshToken,
+            accessToken: config.gmail.accessToken,
+            expires: 1484314697598
         }
     });
 
@@ -44,36 +44,36 @@ module.exports = {
 
   kcSendEmail: function(sendTo, query, callback){
 
-    // login
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: config.gmail.user,
-                clientId: config.gmail.clientId,
-                clientSecret: config.gmail.clientSecret,
-                refreshToken: config.gmail.refreshToken,
-                accessToken: config.gmail.accessToken
-            })
-        }
+
+  var transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+            user: "harleyrowland17@gmail.com",
+            pass: config.gmail.password
+          }
     });
 
     if(query.package == undefined) query.package = "They did not specify a package."
     if(query.body == undefined) query.body = "They sent no body to the message."
 
-    var mailOptions = {
+    var mail = {
+      from: "harley.rowland17@gmail.com",
       to: sendTo,
       subject: "Hog Roast Enquiry for " + query.date,
       text: 'Message from ' + query.name + ' (' + query.email+ ') \n\n\r\r - Telephone number: ' + query.number + '\n\n\r\r' + query.body + '\n\n\r\r' + query.package,
       html: 'Message from ' + query.name + ' (' + query.email+ ') \n\n\r\r - Telephone number: ' + query.number + '\n\n\r\r' + query.body + '\n\n\r\r' + query.package,
     };
 
-    transporter.sendMail(mailOptions, function(err, info){
+
+    transporter.sendMail(mail, function(err, info) {
       if(err){
           console.log(err);
           return callback(err);
       }else{
           console.log('Message sent: ' + info.response);
+          transporter.close();
           return callback(httpStatus.OK);
       }
     });
@@ -82,22 +82,20 @@ module.exports = {
 
     // login
     var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            xoauth2: xoauth2.createXOAuth2Generator({
-                user: config.gmail.user,
-                clientId: config.gmail.clientId,
-                clientSecret: config.gmail.clientSecret,
-                refreshToken: config.gmail.refreshToken,
-                accessToken: config.gmail.accessToken
-            })
-        }
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+            user: "harleyrowland17@gmail.com",
+            pass: config.gmail.password
+          }
     });
 
     if(query.package == undefined) query.package = "They did not specify a package."
     if(query.body == undefined) query.body = "They sent no body to the message."
 
     var mailOptions = {
+      from: "harley.rowland17@gmail.com",
       to: sendTo,
       subject: query.subject,
       text: 'Message from ' + query.name + ' (' + query.email+ ') \n\n\r\r - Telephone number: ' + query.number + '\n\n\r\r' + query.body,
